3197dd1d2e556bbb7b4d28b6ee9eb40c088f69fb
Zayar397 <zayartun0880@gmail.com>
2026-09-13 22:10:45 +0630

    Add project files.

---
+++ Task.Data/Data/HRMSDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Task.Data.Models;
+
+namespace Task.Data.Data;
+
+public partial class HRMSDbContext : DbContext
+{
+    public HRMSDbContext()
+    {
+    }
+
+    public HRMSDbContext(DbContextOptions<HRMSDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TaskItem> TaskItems { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source = DESKTOP-BPF6HTF\\SQL2022; Initial Catalog = HRMS; User Id = sa; Password = p@ssw0rd; Trust Server Certificate = True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskItem>(entity =>
+        {
+            entity.ToTable("TaskItem");
+
+            entity.Property(e => e.Description).HasMaxLength(500);
+            entity.Property(e => e.DueDate).HasColumnType("datetime");
+            entity.Property(e => e.Priority).HasMaxLength(10);
+            entity.Property(e => e.Status).HasMaxLength(10);
+            entity.Property(e => e.Title).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Task.Data/Models/TaskItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task.Data.Models;
+
+public partial class TaskItem
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string Status { get; set; } = null!;
+
+    public string Priority { get; set; } = null!;
+
+    public DateTime DueDate { get; set; }
+}
